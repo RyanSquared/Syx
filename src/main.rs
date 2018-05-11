04f39cb1b42bfef1b0a303be7a24b577077c1301
RyanSquared <vandor2012@gmail.com>
2018-05-11 16:48:52 -0500

    src/main: Add example of using undump

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
-fn main() {
-    println!("Hello, world!");
+pub mod undump;
+
+use std::fs::File;
+
+fn main() -> std::io::Result<()> {
+    let args: Vec<_> = ::std::env::args().collect();
+    match args.get(1) {
+        None => {
+            panic!("Usage: {} [filename]", args.get(0).unwrap());
+        },
+        Some(file) => {
+            let handle = File::open(file)?;
+            let mut state = undump::LoadState::from_read(handle, file);
+            state.check_header();
+            println!("test!");
+            Ok(())
+        }
+    }
 }
