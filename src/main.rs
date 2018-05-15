16bd4c3a231c6c98ecefa655faa8d11d4001cca7
RyanSquared <vandor2012@gmail.com>
2018-05-15 16:11:58 -0500

    src/main: Print values from procedure

--- src/main.rs
+++ src/main.rs
@@ -4,10 +4,11 @@ mod object;
 mod state;
 
 use std::fs::File;
+use object::SyxValue;
 
 fn main() -> std::io::Result<()> {
     let args: Vec<_> = ::std::env::args().collect();
-    match args.get(1) {
+    let main_chunk = match args.get(1) {
         None => {
             panic!("Usage: {} [filename]", args.get(0).unwrap());
         },
@@ -15,9 +16,55 @@ fn main() -> std::io::Result<()> {
             let handle = File::open(file)?;
             match undump::LoadState::from_read(handle, file.clone()) {
                 Err(err) => panic!("fail! => {}", err),
-                Ok(_state) => ()
+                Ok(main_chunk) => main_chunk
             }
-            Ok(())
+        }
+    };
+    if main_chunk.constants.len() > 0 {
+        println!("");
+        println!("constants:");
+        for constant in main_chunk.constants {
+            match constant {
+                SyxValue::Bool(boolean) => {
+                    println!("bool: {}", boolean);
+                },
+                SyxValue::Number(n) => {
+                    println!("number: {}", n);
+                },
+                SyxValue::Integer(n) => {
+                    println!("integer: {}", n);
+                },
+                SyxValue::String(s) => {
+                    match String::from_utf8(s.clone()) {
+                        Ok(string) => println!("string: {}", string),
+                        Err(_) => println!("vec<u8>: {:?}", s),
+                    }
+                }
+                _ => {
+                    ()
+                }
+            }
         }
     }
+    if main_chunk.locvars.len() > 0 {
+        println!("");
+        println!("locals:");
+        for local in main_chunk.locvars {
+            if let Ok(name) = String::from_utf8(local.varname) {
+                println!("local: {}", name)
+            }
+        }
+    }
+    if main_chunk.upvalues.len() > 0 {
+        println!("");
+        println!("upvalues:");
+        for upval in main_chunk.upvalues {
+            if upval.name == vec![] {
+                println!("instack: {}, idx: {}", upval.instack, upval.idx);
+            } else if let Ok(string) = String::from_utf8(upval.name) {
+                println!("{} [{}, {}]", string, upval.instack, upval.idx);
+            }
+        }
+    }
+    Ok(())
 }
